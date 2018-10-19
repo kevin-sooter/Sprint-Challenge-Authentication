6dc4d8143807e654b4c5bc7374c14b26deda37dd
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-10-19 10:44:28 -0500

    Added routes
    Everything is working on Postman
    Moving onto client side

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
-const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig.js');
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,12 +10,48 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+// const token = generateToken(user);
+
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 12);
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      db('users')
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, username: user.username, token });
+        })
+        .catch(err => {
+          if (err.errno === 19) {
+            res
+              .status(400)
+              .json({ error: 'Please provide a unique username.' });
+          } else res.status(500).json(err);
+        });
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password.' });
+      }
+    })
+    .catch(err => res.status(500).json(err));
 }
 
 function getJokes(req, res) {
